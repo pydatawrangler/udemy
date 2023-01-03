8929715fd8d02ac2be27f9b9cbbcbf6515a0468a
Tom Madsen <tmadutah@gmail.com>
2023-01-02 20:20:51 -0700

    web scraping 2022 andrade through section 3

---
+++ web_scraping_2022_andrade/scrape_multiple_files_bs4.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+
+root = 'https://subslikescript.com'
+website = f'{root}/movies_letter-A'
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, 'lxml')
+
+# pagination
+pagination = soup.find('ul', class_='pagination')
+pages = pagination.find_all('li', class_='page-item')
+last_page = pages[-2].text
+
+links = []
+for page in range(1, int(last_page)+1)[:2]:
+    result = requests.get(f'{website}?page={page}')
+    content = result.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    box = soup.find('article', class_="main-article")
+
+    
+    for link in box.find_all('a', href=True):
+        links.append(link['href'])
+        
+    # print(links)
+
+    for link in links:
+        try:
+            print(link)
+            result = requests.get(f'{root}/{link}')
+            content = result.text
+            soup = BeautifulSoup(content, 'lxml')
+            
+            box = soup.find('article', class_="main-article")
+            
+            title = box.find('h1').get_text()
+            transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+        
+            with open(f'transcript_files/{title}.txt', 'w') as file:
+                file.write(transcript)
+        
+        except:
+            print('Link not working')
+            print(link)
+        
+        
+        
--- web_scraping_2022_andrade/web_scraping_2022_andrade_notes.md
+++ web_scraping_2022_andrade/web_scraping_2022_andrade_notes.md
@@ -1,1 +1,15 @@
 Purchased the course on 12/29/2022.
+
+Example websites:
+* https://subslikescript.com/
+
+Recommended to find elements in this order:
+1. ID
+2. Class name
+3. Tag name, CSS Selector
+4. Xpath
+
+Beautiful Soup Steps:
+1. Fetch the pages with `result = requests.get("www.google.com")`
+2. Page content with `content = result.text`
+3. Create soup with `soup = BeautifulSoup(content, "lxml")`
